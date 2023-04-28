4f7f1d3c085687832743d34240a7f27371373eac
Jonathan Bruckman <jbruckman86@gmail.com>
2023-04-28 13:47:20 -0400

    fixed auth bug, login is now working correctly, image upload working

--- components/models/EditModel.tsx
+++ components/models/EditModel.tsx
@@ -6,6 +6,7 @@ import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import Model from "../Model";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModel = () => {
 
@@ -55,6 +56,18 @@ const EditModel = () => {
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
+            <ImageUpload 
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label="Upload profile image"
+            />
+            <ImageUpload 
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label="Upload cover image"
+            />
             <Input 
                 placeholder="Name"
                 onChange={(e) => setName(e.target.value)}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,21 +1,21 @@
-import bcrypt from 'bcrypt';
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import bcrypt from "bcrypt"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-import prisma from "@/libs/prismadb";
+import prisma from "@/libs/prismadb"
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         CredentialsProvider({
             name: 'credentials',
             credentials: {
                 email: { label: 'email', type: 'text' },
-                password: { label: 'password', type: 'password' },
+                password: { label: 'password', type: 'password' }
             },
             async authorize(credentials) {
-                if(!credentials?.email || !credentials?.password) {
+                if (!credentials?.email || !credentials?.password) {
                     throw new Error('Invalid credentials');
                 }
 
@@ -32,7 +32,7 @@ export default NextAuth({
                 const isCorrectPassword = await bcrypt.compare(
                     credentials.password,
                     user.hashedPassword
-                )
+                );
 
                 if (!isCorrectPassword) {
                     throw new Error('Invalid credentials');
@@ -44,10 +44,12 @@ export default NextAuth({
     ],
     debug: process.env.NODE_ENV === 'development',
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt',
     },
     jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET
+        secret: process.env.NEXTAUTH_JWT_SECRET,
     },
-    secret: process.env.NEXTAUTH_SECRET
-});
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
